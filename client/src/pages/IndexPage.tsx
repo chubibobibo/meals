eab99357d23383fbb1ad94bc6ace0dd1109266c1
Alistair Abao <abaolester@gmail.com>
2026-06-02 17:15:53 +0200

    added background image to index page

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -1,8 +1,15 @@
 function IndexPage() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <main className='w-screen h-screen'>
+      <img
+        src='../src/assets/mealappbg.png'
+        alt='bg-image'
+        className='w-screen h-screen opacity-50 absolute object-cover object-left'
+      />
+      <h1 className='relative font-bold text-black'>HELLO</h1>
+
+      {/* <img src='../src/assets/hero.png' alt='' /> */}
+    </main>
   );
 }
 export default IndexPage;
