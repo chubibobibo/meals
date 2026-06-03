51cc48651df637aedf57d735a236ee120350c918
Alistair Abao <abaolester@gmail.com>
2026-06-03 18:32:13 +0200

    layout changes and effects in the landing page

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -10,11 +10,14 @@ function AvatarComponent({ avatarName, imgLink }: AvatarComponentType) {
   };
 
   return (
-    <section className='flex flex-col items-center' onClick={handleClick}>
+    <section
+      className='flex flex-col items-center cursor-pointer'
+      onClick={handleClick}
+    >
       <img
         src={imgLink}
         alt=''
-        className='w-25 h-25 rounded-full -mt-20 object-cover object-top'
+        className='w-25 h-25 object-cover object-top rounded-full border-6 hover:scale-115 active:opacity-80'
       />
       <h1 className='font-semibold text-lg'>{avatarName}</h1>
     </section>
